4e4ad8fbe3bfedd20fb577c31690f9e4bce2ef27
Muzammil Ali <shanikbs6242@gmail.com>
2023-04-30 21:39:00 +0500

    new commit

--- src/pages/components/Projects.js
+++ src/pages/components/Projects.js
@@ -2,10 +2,12 @@ import React from 'react'
 import Image from 'next/image'
 import img1 from '../components/images/weather.png'
 import img2 from '../components/images/calculator.png'
+import img3 from '../components/images/youtube.png'
+import img4 from '../components/images/clock.png'
 
 const Projects = () => {
     return (
-        <div className='flex m-8'>
+        <div className='flex m-8 flex-wrap'>
 
             <div className='btn w-[180px] h-[200px] md:w-[280px] md:h-[300px] hover:border-white  border-2 border-purple-900 rounded-md my-4 m-2 '>
                 <div className='flex flex-col justify-around align-middle h-full transition-all   '>
@@ -21,7 +23,25 @@ const Projects = () => {
                     <Image src={img2} className='mx-auto ' height={100} width={100} alt='img1' />
 
                     <h2 className='text-purple-500 text-3xl font-bold text-center'> 3d Calculator  </h2>
-                    <a href='https://cal3d.netlify.app/' target='_blank' className='underline-offset-4 text-center text-blue-400 hover:text-white '> <u>Visit weather App</u></a>
+                    <a href='https://cal3d.netlify.app/' target='_blank' className='underline-offset-4 text-center text-blue-400 hover:text-white '> <u>Visit Calculator</u></a>
+                </div>
+
+            </div>
+             <div className='btn w-[180px] h-[200px] md:w-[280px] md:h-[300px] hover:border-white  border-2 border-purple-900 rounded-md my-4 m-2 '>
+                <div className='flex flex-col justify-around align-middle h-full transition-all   '>
+                    <Image src={img3} className='mx-auto ' height={100} width={100} alt='img1' />
+
+                    <h2 className='text-purple-500 text-3xl font-bold text-center'> Youtube clone </h2>
+                    <a href='https://mtubeclone.netlify.app/' target='_blank' className='underline-offset-4 text-center text-blue-400 hover:text-white '> <u>Visit Youtube clone</u></a>
+                </div>
+
+            </div>
+             <div className='btn w-[180px] h-[200px] md:w-[280px] md:h-[300px] hover:border-white  border-2 border-purple-900 rounded-md my-4 m-2 '>
+                <div className='flex flex-col justify-around align-middle h-full transition-all   '>
+                    <Image src={img4} className='mx-auto ' height={100} width={100} alt='img1' />
+
+                    <h2 className='text-purple-500 text-3xl font-bold text-center'> clock  </h2>
+                    <a href='https://clockpk.netlify.app/' target='_blank' className='underline-offset-4 text-center text-blue-400 hover:text-white '> <u>Visit Clock</u></a>
                 </div>
 
             </div>
